1a6ad81740a2c2f4ad73b8cd55ec321e2ef049a6
Earlymikoman <xboosinger@gmail.comgit config --global user.email xboosing>
2026-03-29 15:15:46 -0700

    promptmetadata

--- obj/Debug/net8.0/azure-file-server.AssemblyInfo.cs
+++ obj/Debug/net8.0/azure-file-server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("azure-file-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+289e722b5d9dd2dd348f41c91432dbbc1e8eb999")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eadf949599082b2c0b7de73b02ad122ecfe85ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("azure-file-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("azure-file-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
