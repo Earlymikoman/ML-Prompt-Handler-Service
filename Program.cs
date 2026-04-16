730323c9f8f4c46eea5ed691158abe69b7413fa1
Earlymikoman <xboosinger@gmail.comgit config --global user.email xboosing>
2026-04-16 09:29:49 -0700

    oops, forgot FindPromptName

--- Program.cs
+++ Program.cs
@@ -61,6 +61,7 @@ class Program
         app.MapGet("/listprompts", instance.ListPromptsDelegate);
         app.MapGet("/deleteprompt", instance.DeletePromptDelegate);
         app.MapPost("/uploadprompt", instance.UploadPromptDelegate);
+        app.MapGet("/findpromptname", instance.FindPromptNameDelegate);
 
         app.Run();
     }
